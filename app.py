edca42bbdb2e6fa5a52f3546c18262add23611a7
SimonaMnv <simona.mnv@gmail.com>
2022-04-05 00:53:49 +0300

    attempt to deploy

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from twisted.internet import reactor
 from scrapy.crawler import CrawlerRunner
 from scrapy.utils.log import configure_logging
 
-from scrapy.settings import Settings
+from scrapy.utils.project import get_project_settings
 from salaryscrape.salaryscrape.spiders.glassdoor_spider import GlassDoor
 
 app = Flask(__name__)
@@ -19,13 +19,10 @@ def index():
 
 
 @app.route('/crawl', methods=['GET', 'POST'])
-def get_data(response):
-    settings = Settings()
-    print("Proxy IP:", response.headers)
-
+def get_data():
     configure_logging({'LOG_FORMAT': '%(levelname)s: %(message)s'})
 
-    runner = CrawlerRunner(settings)
+    runner = CrawlerRunner(get_project_settings)
     d = runner.crawl(GlassDoor)
     d.addBoth(lambda _: reactor.stop())
     reactor.run()
