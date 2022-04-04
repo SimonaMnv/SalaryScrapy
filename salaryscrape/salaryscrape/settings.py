8f9ba8a969b2d657f0c5c07d20acc27c824f5ba6
SimonaMnv <simona.mnv@gmail.com>
2022-04-05 01:23:13 +0300

    attempt to deploy

--- salaryscrape/salaryscrape/settings.py
+++ salaryscrape/salaryscrape/settings.py
@@ -6,7 +6,7 @@
 #     https://docs.scrapy.org/en/latest/topics/settings.html
 #     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-from ..utils.proxies import get_proxies
+from utils.proxies import get_proxies
 
 BOT_NAME = 'salaryscrape'
 
