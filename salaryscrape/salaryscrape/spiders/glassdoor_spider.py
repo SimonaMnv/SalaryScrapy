7877be3e4d3c32c9f596acb95f35d03c099e6624
SimonaMnv <simona.mnv@gmail.com>
2022-04-05 02:48:09 +0300

    attempt to deploy

--- salaryscrape/salaryscrape/spiders/glassdoor_spider.py
+++ salaryscrape/salaryscrape/spiders/glassdoor_spider.py
@@ -6,6 +6,7 @@ from scrapy.spiders.init import InitSpider
 from ..items import CompanySalary
 
 import logging
+import re
 
 from .secrets_config import config
 
@@ -65,7 +66,8 @@ class GlassDoor(InitSpider):
         main_page = json.loads(response.xpath('//script[@type="application/ld+json"]//text()').extract_first())
 
         salary['timestamp'] = str(datetime.datetime.now())
-        salary['location'] = main_page["occupationLocation"][0]["name"]
+        salary['location'] = main_page["occupationLocation"][0]["name"] if main_page["occupationLocation"][0]["name"] \
+            else re.search('(?:.*?\/){4}([^\/-]+)', str(response.url))[1]
         salary['job_title'] = main_page["name"]
         salary['job_percentile10_payment'] = main_page["estimatedSalary"][0]["percentile10"]
         salary['job_median_payment'] = main_page["estimatedSalary"][0]["median"]
