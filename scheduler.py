e37040f0b5f8c562d63881a83226565f6ac52090
SimonaMnv <simona.mnv@gmail.com>
2022-04-04 21:56:05 +0300

    change time to test

--- scheduler.py
+++ scheduler.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     subprocess.run("scrapyd-deploy", shell=True, universal_newlines=True)
 
     scheduler = TwistedScheduler(timezone=pytz.timezone('UTC'))
-    scheduler.add_job(send_request, 'cron', day_of_week='monday', hour=6, minute=0)
+    scheduler.add_job(send_request, 'cron', day_of_week='mon-fri', minute='*/2')
     scheduler.start()
 
     reactor.run()
