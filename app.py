8681862fd85682097eee132c5bb0d83981a66a24
SimonaMnv <simona.mnv@gmail.com>
2022-04-05 03:37:45 +0300

    attempt to deploy

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 from flask_apscheduler import APScheduler
+import datetime
 
 from flask import Flask, request, jsonify
 from twisted.internet import reactor
@@ -11,10 +12,9 @@ from salaryscrape.salaryscrape.spiders.glassdoor_spider import GlassDoor
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def index():
-    return jsonify({'ip': request.remote_addr}), 200
+scheduler = APScheduler()
+scheduler.init_app(app)
+scheduler.start()
 
 
 def crawl():
@@ -28,10 +28,16 @@ def crawl():
     return 'Scraping...'
 
 
-if __name__ == '__main__':
-    scheduler = APScheduler()
-    scheduler.init_app(app)
-    scheduler.start()
+@app.route('/')
+def index():
+    return jsonify({'ip': request.remote_addr}), 200
 
+
+@app.route('/schedule_crawl')
+def schedule_crawl():
     scheduler.add_job(id='glassdoor_crawl_job', func=crawl, trigger='interval', hour=2)
+    return jsonify({str(datetime.datetime.now()): 'Crawling Scheduled'}), 200
+
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT')))
