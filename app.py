263605590e936367c93d9612203a4ee1a71cef69
SimonaMnv <simona.mnv@gmail.com>
2022-04-05 10:35:37 +0300

    attempt to deploy

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from flask_apscheduler import APScheduler
 import datetime
 
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify
 from twisted.internet import reactor
 from scrapy.crawler import CrawlerRunner
 from scrapy.utils.log import configure_logging
@@ -27,8 +27,6 @@ class Config(object):
 
 
 app = Flask(__name__)
-# app.config.from_object(Config())
-
 scheduler = APScheduler()
 scheduler.init_app(app)
 scheduler.start()
@@ -48,7 +46,7 @@ def crawl():
 @app.route('/crawl')
 def add_tasks():
     app.apscheduler.add_job(func=crawl, trigger='cron', minute='*/5', id='job-crawler')
-    return jsonify({str(datetime.datetime.now()) + '_' + 'crawl job started'}), 200
+    return jsonify({str(datetime.datetime.now()): 'crawl job started'}), 200
 
 
 if __name__ == '__main__':
