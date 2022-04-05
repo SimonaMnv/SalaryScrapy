06ff2905a093f6649fef48b8b7cbb13e1a0e9adf
SimonaMnv <simona.mnv@gmail.com>
2022-04-05 11:13:31 +0300

    attempt to deploy

--- app.py
+++ app.py
@@ -1,35 +1,48 @@
+import crochet
+
+crochet.setup()
+
 import os
 from flask_apscheduler import APScheduler
 import datetime
 
 from flask import Flask, jsonify
-from twisted.internet import reactor
 from scrapy.crawler import CrawlerRunner
 from scrapy.utils.log import configure_logging
-
+from scrapy.signalmanager import dispatcher
+from scrapy import signals
 from scrapy.utils.project import get_project_settings
-from salaryscrape.salaryscrape.spiders.glassdoor_spider import GlassDoor
 
+from salaryscrape.salaryscrape.spiders import glassdoor_spider
 
 app = Flask(__name__)
+
 scheduler = APScheduler()
 scheduler.init_app(app)
 scheduler.start()
 
+crawl_runner = CrawlerRunner(get_project_settings())
 
-def crawl():
-    configure_logging({'LOG_FORMAT': '%(levelname)s: %(message)s'})
 
-    runner = CrawlerRunner(get_project_settings())
-    glassdoor_spider = runner.crawl(GlassDoor)
-    glassdoor_spider.run()
+@app.route("/scrape")
+def scrape():
+    scrape_with_crochet()
+    return jsonify({str(datetime.datetime.now()): 'started scrape_with_crochet()'})
 
-    return str(datetime.datetime.now()) + '_' + 'Scraping...'
+
+@crochet.wait_for(timeout=60.0)
+def scrape_with_crochet():
+    """
+    signal fires when single item is processed and calls _crawler_result to append that item
+    returns a twisted.internet.defer.Deferred
+    """
+    eventual = crawl_runner.crawl(glassdoor_spider.GlassDoor)
+    return eventual
 
 
 @app.route('/crawl')
 def add_tasks():
-    app.apscheduler.add_job(func=crawl, trigger='cron', minute='*/5', id='job-crawler')
+    app.apscheduler.add_job(func=scrape, trigger='cron', minute='*/5', id='job-crawler')
     return jsonify({str(datetime.datetime.now()): 'crawl job started'}), 200
 
 
