b5d0853e1ea62cf74f16b4a557f52a80b2c8e6ed
SimonaMnv <simona.mnv@gmail.com>
2022-04-07 18:37:58 +0300

    attempt to deploy

--- app.py
+++ app.py
@@ -14,14 +14,15 @@ scheduler.start()
 
 def run_spider():
     """ run the spider """
-    subprocess.run(['cd', 'salaryscrape/salaryscrape'], shell=True)
-    print("Current dir", subprocess.check_output(['ls', '-l'], shell=True))
+    subprocess.run(['cd', 'salaryscrape'], shell=True)
+    subprocess.run(['cd', 'salaryscrape'], shell=True)
+    subprocess.check_output(['ls', '-l'], shell=True)
     subprocess.run(['scrapy', 'crawl', 'glassdoor_spider'], shell=True)
 
 
 @app.route('/crawl')
 def add_tasks():
-    """ create a scheduler to execute the spider weekly """
+    """ create a scheduler to execute the spider weekly - one unique id running at a time """
     app.apscheduler.add_job(func=run_spider, trigger='cron', minute='*/1', id='glassdoor_spider_crawl_job')
     return jsonify({str(datetime.datetime.now()): 'crawl job started'}), 200
 
