dcb7ca8f836d5b0e24eae2181eed10a3118f428f
SimonaMnv <simona.mnv@gmail.com>
2022-04-14 19:51:05 +0300

    create a home page

--- app.py
+++ app.py
@@ -17,17 +17,15 @@ def home():
     return jsonify({str(datetime.datetime.now()): ' '}), 200
 
 
-@app.route('/crawl')
 def run_spider():
     """ run the spider inside the heroku container """
     call(["scrapy", 'crawl', 'glassdoor_spider'], cwd='/app/salaryscrape')
-    return jsonify({str(datetime.datetime.now()): 'crawl job started'}), 200
 
 
-@app.route('/scheduled_crawl')
+@app.route('/crawl')
 def add_tasks():
     """ create a scheduler to execute the spider weekly - one unique id running at a time """
-    app.apscheduler.add_job(func=run_spider, trigger='cron', day_of_week='wed', hour='20', minute='01',
+    app.apscheduler.add_job(func=run_spider, trigger='cron', day_of_week='thu', hour='20', minute='01',
                             id='glassdoor_spider_crawl_job')
     return jsonify({str(datetime.datetime.now()): 'crawl job started'}), 200
 
