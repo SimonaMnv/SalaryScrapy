e00861b70770db5c3a6be5d224f51b096a780ca4
SimonaMnv <simona.mnv@gmail.com>
2022-06-07 16:02:55 +0300

    fix tests

--- tests/external_apis_unit_test.py
+++ tests/external_apis_unit_test.py
@@ -8,19 +8,20 @@ from salaryscrape.salaryscrape.utils.secrets_config import config
 
 class CheckExternalAPIs(unittest.TestCase):
     def setUp(self):
+        """ setup the api key for the lat/lon api """
         self.cords_api = f"http://api.positionstack.com/v1/forward?access_key={config['positionstack_api_key']}&query="
         print("setUp is running")
 
     def test_currency_api(self):
         """ check that the https://api.exchangerate.host is available. this converts 1 eur to eur """
         api_status = local_revenue_to_eur('eur', '1', '2022-06-07')
-        self.assertTrue(api_status == 1)
+        self.assertEquals(api_status, 1)
 
     def test_lat_lon_api(self):
-        """ check that the api lat/lon api is available. check that greece's lat is 39.78331"""
+        """ check that the api lat/lon api is available. check that greece's lat is 39.78331 """
         r = requests.get(self.cords_api + 'greece')
         lat_lon = r.json()
-        self.assertTrue(lat_lon['data'][0]['latitude'] == '39.78331')
+        self.assertEquals(lat_lon['data'][0]['latitude'], 39.78331)
 
     def tearDown(self) -> None:
         """ bye bye """
